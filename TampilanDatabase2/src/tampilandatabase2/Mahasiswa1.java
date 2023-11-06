cf9e188f80b68044c31eb3897ef16b4f4dd4391b
irkhmnwn <149305559+irkhmnwn@users.noreply.github.com>
2023-11-06 20:58:05 +0700

    Add files via upload

---
+++ TampilanDatabase2/src/tampilandatabase2/Mahasiswa1.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tampilandatabase2;
+
+/**
+ *
+ * @author bayu
+ */
+public class Mahasiswa1 {
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the nim
+     */
+    public String getNim() {
+        return nim;
+    }
+
+    /**
+     * @param nim the nim to set
+     */
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    private String nim;
+    private String nama;
+    private String alamat;
+}
